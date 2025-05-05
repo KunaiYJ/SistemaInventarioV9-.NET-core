adf26b517736f34de10f096e34a589071df03d6a
Isac Montes <IsacMontes123@hotmai.com>
2025-05-05 11:32:45 -0700

    CRUD de Marca

--- SistemaInventarioV9/obj/Debug/net9.0/SistemaInventarioV9.AssemblyInfo.cs
+++ SistemaInventarioV9/obj/Debug/net9.0/SistemaInventarioV9.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventarioV9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d744f57dff138fdf11d375150704b677dba092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d365894e89d4ea88469107c9fe49a3ba279f6243")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventarioV9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventarioV9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
